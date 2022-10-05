208045c1ddbc75525607263633b7e57740645a80
adarsh <adarsh@pop-os.localdomain>
2022-10-05 21:11:20 +0545

    created a new cmake libary build

---
+++ build/CMakeFiles/cmake_hello.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/adarsh/Desktop/learning/cmake/lib/math/operations.cpp" "CMakeFiles/cmake_hello.dir/lib/math/operations.cpp.o" "gcc" "CMakeFiles/cmake_hello.dir/lib/math/operations.cpp.o.d"
+  "/home/adarsh/Desktop/learning/cmake/main.cpp" "CMakeFiles/cmake_hello.dir/main.cpp.o" "gcc" "CMakeFiles/cmake_hello.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cmake_hello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_hello.dir/lib/math/operations.cpp.o"
+  "CMakeFiles/cmake_hello.dir/lib/math/operations.cpp.o.d"
+  "CMakeFiles/cmake_hello.dir/main.cpp.o"
+  "CMakeFiles/cmake_hello.dir/main.cpp.o.d"
+  "bin/cmake_hello"
+  "bin/cmake_hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
