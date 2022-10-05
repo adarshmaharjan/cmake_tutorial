d6153e65f68be2ee1cb12eb1fbf41a96186c274b
adarsh <adarsh@pop-os.localdomain>
2022-10-05 20:54:53 +0545

    initial commit

---
+++ build/CMakeFiles/cmake_hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_hello.dir/main.cpp.o"
+  "CMakeFiles/cmake_hello.dir/main.cpp.o.d"
+  "bin/cmake_hello"
+  "bin/cmake_hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
